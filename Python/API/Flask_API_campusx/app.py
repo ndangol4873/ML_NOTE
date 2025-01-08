664f5b9d594acb5105c13fc11c917cfc13fc957b
ndangol12619 <nareshkadambi@gmail.com>
2025-01-07 22:14:26 -0500

    Flask API Wrape up

--- Python/API/Flask_API_campusx/app.py
+++ Python/API/Flask_API_campusx/app.py
@@ -10,10 +10,24 @@ def home():
     return 'Hello World'
 
 
-@app.route('/api/teams')
-def teams():
-    teams = ipl.teamsAPI()
+@app.route('/api/teams_name')
+def teams_name():
+    teams = ipl.teamsNameAPI()
     return jsonify(teams)
+
+
+@app.route('/api/teams_record')
+def teams_record():
+    team_name = request.args.get('team')
+    response = ipl.teamsRecordsAPI(team_name)
+    return response
+
+
+@app.route('/api/batting_record')
+def batting_record():
+    batsman_name = request.args.get('batsman')
+    response = ipl.batsmanAPI(batsman_name)
+    return response
     
 
 ## /api/teamvteam?team1=Rajasthan Royals&team2=Royal Challengers Bangalore
@@ -25,6 +39,17 @@ def teamvteam():
     response = ipl.teamVteamAPI(team1,team2)
     print(response)
     return jsonify(response)
+
+
+## Balls API Test
+@app.route('/api/balls')
+def balls():
+    response = ipl.ball_record_count()
+    if response:
+        return response
+    
+
+# @app.route('/api/team-records')
 
 
 app.run(debug = True)
--- Python/API/Flask_API_campusx/ipl.py
+++ Python/API/Flask_API_campusx/ipl.py
@@ -1,24 +1,173 @@
 import numpy as np
 import pandas as pd
+from flask import jsonify
+import json
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+    
 
 
-ipl_matches = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRy2DUdUbaKx_Co9F0FSnIlyS-8kp4aKv_I0-qzNeghiZHAI_hw94gKG22XTxNJHMFnFVKsO4xWOdIs/pub?gid=1655759976&single=true&output=csv"
-matches = pd.read_csv(ipl_matches)
+matches = pd.read_csv('d:/ML_NOTE_DATASET/ipl-matches.csv')
+balls = pd.read_csv('d:/ML_NOTE_DATASET/IPL_Ball_by_Ball_2008_2022.csv')
+
+
+
+ball_withmatch = balls.merge(matches, on='ID', how='inner').copy()
+
+bowling_team = []
+for index, (team1, battingteam) in enumerate(zip(ball_withmatch['Team1'], ball_withmatch['BattingTeam'])):
+    if team1 == battingteam:
+        bowling_team.append(np.nan)
+    else:
+        bowling_team.append(team1)
+
+ball_withmatch['BowlingTeam'] = bowling_team
+ball_withmatch['BowlingTeam'] = ball_withmatch['BowlingTeam'].fillna(ball_withmatch['Team2'])
+batter_data = ball_withmatch[['BowlingTeam','BattingTeam','Player_of_Match']]
 
 
 
 
-def teamsAPI():
+def team1vsteam2(team, team2):
+    df = matches[((matches['Team1'] == team) & (matches['Team2'] == team2)) | (
+                (matches['Team2'] == team) & (matches['Team1'] == team2))].copy()
+    mp = df.shape[0]
+    won = df[df.WinningTeam == team].shape[0]
+    nr = df[df.WinningTeam.isnull()].shape[0]
+    loss = mp - won - nr
+
+    return {'matchesplayed': mp,
+            'won': won,
+            'loss': loss,
+            'noResult': nr}
+
+
+
+
+def ball_record_count():
+    row = {
+        'Total Bowling Data : ':balls.shape[0]
+    }
+    return jsonify(row)
+
+
+
+
+def teamsNameAPI():
+    '''Return  Teams Name '''
     teams = list(set(list(matches['Team1']) + list(matches['Team2'])))
     teams_dict ={
         'teams':teams
     }
-
     return teams_dict
+
+
+def allRecord(team):
+    df = matches[(matches['Team1'] == team) | (matches['Team2'] == team)].copy()
+    mp = df.shape[0]
+    won = df[df.WinningTeam == team].shape[0]
+    nr = df[df.WinningTeam.isnull()].shape[0]
+    loss = mp - won - nr
+    nt = df[(df.MatchNumber == 'Final') & (df.WinningTeam == team)].shape[0]
+    return {'matchesplayed': mp,
+            'won': won,
+            'loss': loss,
+            'noResult': nr,
+            'title': nt}
+
+
+
+def teamsRecordsAPI(team):
+    df = matches[(matches['Team1'] == team) | (matches['Team2'] == team)].copy()
+    self_record = allRecord(team)
+    TEAMS = matches.Team1.unique()
+    against = {team2: team1vsteam2(team, team2) for team2 in TEAMS}
+    data = {team: {'overall': self_record,
+                   'against': against}}
+    return json.dumps(data, cls=NpEncoder)
+
+
+def batsmanVsTeam(batsman, team, df):
+    df = df[df.BowlingTeam == team].copy()
+    return batsmanRecord(batsman, df)
+
+def batsmanAPI(batsman, balls=batter_data):
+    df = balls[balls.innings.isin([1, 2])]  # Excluding Super overs
+    self_record = batsmanRecord(batsman, df=df)
+    TEAMS = matches.Team1.unique()
+    against = {team: batsmanVsTeam(batsman, team, df) for team in TEAMS}
+    data = {
+        batsman: {'all': self_record,
+                  'against': against}
+    }
+    return json.dumps(data, cls=NpEncoder)
+
+bowler_data = batter_data.copy()
+
+
+def batsmanRecord(batsman, df):
+    if df.empty:
+        return np.nan
+    out = df[df.player_out == batsman].shape[0]
+    df = df[df['batter'] == batsman]
+    inngs = df.ID.unique().shape[0]
+    runs = df.batsman_run.sum()
+    fours = df[(df.batsman_run == 4) & (df.non_boundary == 0)].shape[0]
+    sixes = df[(df.batsman_run == 6) & (df.non_boundary == 0)].shape[0]
+    if out:
+        avg = runs / out
+    else:
+        avg = np.inf
+
+    nballs = df[~(df.extra_type == 'wides')].shape[0]
+    if nballs:
+        strike_rate = runs / nballs * 100
+    else:
+        strike_rate = 0
+    gb = df.groupby('ID').sum()
+    fifties = gb[(gb.batsman_run >= 50) & (gb.batsman_run < 100)].shape[0]
+    hundreds = gb[gb.batsman_run >= 100].shape[0]
+    try:
+        highest_score = gb.batsman_run.sort_values(ascending=False).head(1).values[0]
+        hsindex = gb.batsman_run.sort_values(ascending=False).head(1).index[0]
+        if (df[df.ID == hsindex].player_out == batsman).any():
+            highest_score = str(highest_score)
+        else:
+            highest_score = str(highest_score) + '*'
+    except:
+        highest_score = gb.batsman_run.max()
+
+    not_out = inngs - out
+    mom = df[df.Player_of_Match == batsman].drop_duplicates('ID', keep='first').shape[0]
+    data = {
+        'innings': inngs,
+        'runs': runs,
+        'fours': fours,
+        'sixes': sixes,
+        'avg': avg,
+        'strikeRate': strike_rate,
+        'fifties': fifties,
+        'hundreds': hundreds,
+        'highestScore': highest_score,
+        'notOut': not_out,
+        'mom': mom
+    }
+
+    return data
+
 
 
 
 def teamVteamAPI(team1,team2):
+    '''Returns the match details played between two teams'''
     valid_team = list(set(list(matches['Team1']) + list(matches['Team2'])))
 
     if team1 in valid_team and team2 in valid_team:
@@ -36,7 +185,6 @@ def teamVteamAPI(team1,team2):
               team2: str(matches_won_team2),
               'draws': str(draws)
         }
-
         return response
     else:
         return {'message' : 'Invalid Team Name'}
